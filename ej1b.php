f5ba827ff8b888c9a942379ef65c9da4f00a889c
CristinaHur <113576562+CristinaHur@users.noreply.github.com>
2022-09-26 09:50:43 +0200

    Add files via upload

---
+++ ej1b.php
@@ -0,0 +1,20 @@
+<HTML>
+<HEAD><TITLE> EJ1B – Conversor decimal a binario</TITLE></HEAD>
+<BODY>
+<?php
+
+$num= "168";
+$resto=$n%2;
+$n=$num;
+
+
+
+do{
+	$n/2;
+  echo $resto;
+}
+while($n>2);
+   
+?>
+</BODY>
+</HTML>
