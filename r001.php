129e4f695126d875d9259dbfdeb0ede56f01e73a
CristinaHur <113576562+CristinaHur@users.noreply.github.com>
2022-09-27 19:44:16 +0200

    Add files via upload

---
+++ r001.php
@@ -0,0 +1,30 @@
+<HTML>
+<HEAD><TITLE> EJ refuerzo </TITLE></HEAD>
+<BODY>
+
+<?php
+    echo "<table border=1 >";
+    $n=1;
+    for($n1=1;$n1<=9;$n1++){
+        echo "<tr>";
+    for($n2=1;$n2<=10;$n2++){
+        echo "<td>". $n. "</td>";
+        $n=$n+1;
+    } 
+    echo "</tr>";
+    }
+  
+        for($n1=1;$n1<=1;$n1++){
+        $n=90;
+        echo "<tr>";
+   	 for($n2=1;$n2<=10;$n2++){
+        echo "<td>". $n. "</td>";
+        $n=$n+1;
+    } 
+    echo "</tr>";
+    }
+
+?>
+
+</BODY>
+</HTML>
