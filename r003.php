2bf8b25f37a287401053958f6114aa7ae915fc3e
CristinaHur <113576562+CristinaHur@users.noreply.github.com>
2022-09-27 19:55:21 +0200

    Add files via upload

---
+++ r003.php
@@ -0,0 +1,60 @@
+<HTML>
+<HEAD><TITLE> EJ3 refuerzo </TITLE></HEAD>
+<BODY>
+
+<?php
+echo "<table border=1 >";
+$n=2;
+for($n1=1;$n1<=1;$n1++){
+    echo "<tr>";
+for($n2=1;$n2<=10;$n2++){
+    echo "<td>". $n. "</td>";
+    $n=$n+10;
+} 
+echo "</tr>";
+}
+
+    for($n1=1;$n1<=1;$n1++){
+    $n=4;
+    echo "<tr>";
+    for($n2=1;$n2<=10;$n2++){
+    echo "<td>". $n. "</td>";
+    $n=$n+10;
+} 
+echo "</tr>";
+}
+
+    for($n1=1;$n1<=1;$n1++){
+    $n=6;
+    echo "<tr>";
+    for($n2=1;$n2<=10;$n2++){
+    echo "<td>". $n. "</td>";
+    $n=$n+10;
+} 
+echo "</tr>";
+}
+
+    for($n1=1;$n1<=1;$n1++){
+    $n=8;
+    echo "<tr>";
+    for($n2=1;$n2<=10;$n2++){
+    echo "<td>". $n. "</td>";
+    $n=$n+10;
+} 
+echo "</tr>";
+}
+
+    for($n1=1;$n1<=1;$n1++){
+    $n=10;
+    echo "<tr>";
+    for($n2=1;$n2<=10;$n2++){
+    echo "<td>". $n. "</td>";
+    $n=$n+10;
+} 
+echo "</tr>";
+}
+
+?>
+
+</BODY>
+</HTML>
