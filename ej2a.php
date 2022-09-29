a36581155f250e9fb72e72a6a202148d3c4a73a2
CristinaHur <113576562+CristinaHur@users.noreply.github.com>
2022-09-29 10:47:41 +0200

    Update ej2a.php

--- ej2a.php
+++ ej2a.php
@@ -14,12 +14,13 @@ for ( $i=0;$i < 20; $i++){
 //var_dump($impares);
 
 
-$sumap=0;
-$sumai=0;for ( $i=0;$i < 20; $i++){  
-    if($i%2==0)
-	  $sumap=$sumap+$impares[$i];
-  else
-      $sumai=$sumai+$impares[$i];
+$sumap=0;//Para ir acumulado la suma de los valores de las celdas pares
+$sumai=0;//para ir accumulando la suma de los valores de las celdas impares
+for ( $i=0;$i < 20; $i++){  
+    if($i%2==0)//si el valor del indice es par:
+	  $sumap=$sumap+$impares[$i];//se suma ese valor en la suma par 
+  else //Si no lo es (porque es impar)
+      $sumai=$sumai+$impares[$i];//se suma ese valor en la suma impar
 }
 echo "La suma de las celdas pares es: ".$sumap."<BR>";
 echo "La suma de las celdas impares es: ".$sumai."<BR>";
