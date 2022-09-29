a6f6a7159ed78ce338a6420bc51481c99789dc0e
CristinaHur <113576562+CristinaHur@users.noreply.github.com>
2022-09-29 12:09:01 +0200

    Add files via upload

---
+++ ej3a.php
@@ -0,0 +1,38 @@
+<HTML>
+<HEAD><TITLE> EJ3-Arrays unidimensionales </TITLE></HEAD>
+<BODY>
+<?php
+$binarios = array();
+$aux = 1;
+
+
+for ( $i=0;$i < 20; $i++){
+  
+    $binarios[$i] = $aux;
+    $aux += 1;
+}
+
+echo "
+<table border=1>
+    <tr>
+    <th>Indice</th>
+    <th>Binario</th>
+    <th>Octal</th>
+  </tr>";
+
+$cont = 0;
+foreach ($binarios as $bin){
+$bin=decbin($cont);
+$oct=decoct($bin);
+
+ 	echo "<tr>
+    <td>".$cont."</td>
+    <td>".$bin."</td>
+    <td>".$oct."</td>
+  	</tr>";
+    $cont += 1;
+
+}
+?>
+</BODY>
+</HTML>
